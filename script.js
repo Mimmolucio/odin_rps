2cba74ab2f035a2727eb9a75767b87610a7a5105
Mimmolucio <domenico.lp@gmail.com>
2026-02-03 12:28:32 +0100

    finished playRound

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice)
+    console.log(computerChoice)
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You Lose! Paper beats Rock.");
+        ++computerScore;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You Win! Rock beats Scissors.");
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You Lose! Scissors beat Paper.");
+        ++computerScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You Win! Paper beats Rock.");
+        ++humanScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You Lose! Rock beats Scissors.");
+        ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You Win! Scissors beat Paper.");
+        ++humanScore;
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a Tie!")
+    }
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
